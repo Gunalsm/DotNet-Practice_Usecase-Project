250274f025cdabbc826bad6b6f8a1d6f3beb61f1
gunalansureshsm5598@gmail.com <gunalansm5598@gmail.com>
2023-09-06 11:57:40 +0530

    Initial Commit

---
+++ BethanysPieShop/Program.cs
@@ -0,0 +1,60 @@
+using BethanysPieShop.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("BethanysPieShopDbContextConnection") ?? throw new InvalidOperationException("Connection string 'BethanysPieShopDbContextConnection' not found.");
+
+builder.Services.AddControllersWithViews()
+     .AddJsonOptions(options =>
+     {
+         options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+     }); ;
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IPieRepository, PieRepository>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+
+builder.Services.AddScoped<IShoppingCart, ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+builder.Services.AddSession();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+builder.Services.AddDbContext<BethanysPieShopDbContext>(options => {
+    options.UseSqlServer(
+        builder.Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]);
+});
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<BethanysPieShopDbContext>();
+
+
+var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseStaticFiles();
+app.UseSession();
+app.MapDefaultControllerRoute();
+
+/*app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");*/
+
+
+DbInitializer.Seed(app);
+app.MapRazorPages();
+app.MapBlazorHub();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapFallbackToPage("/app/{*catchall}", "/App/Index");
+
+
+app.Run();
